25159bc82cafa925f34061786be3fe6d8000014e
Lucas Celestino <lucas.celp1@gmail.com>
2023-02-22 23:17:59 -0300

    Mudando senha bd

--- database/Database.js
+++ database/Database.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('bancoRoot','root','lucas@98623457',{
-    host: 'localhost',
+const connection = new Sequelize('railway','root','8ZpEgitN4pAx0Avwbui4',{
+    host: 'containers-us-west-83.railway.app',
     dialect: 'mysql'
 });
 module.exports = connection;
+// git add *
+// git push
